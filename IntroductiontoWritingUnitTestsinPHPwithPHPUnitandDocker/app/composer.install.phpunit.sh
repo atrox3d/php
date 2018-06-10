b295cba9a868c47acb3b4596febdde034cf22040
atrox3d <atrox3d@gmail.com>
2018-06-10 20:08:27 +0200

    add tests, fix timer

---
+++ IntroductiontoWritingUnitTestsinPHPwithPHPUnitandDocker/app/composer.install.phpunit.sh
@@ -0,0 +1,1 @@
+docker run --rm -v $(pwd):/app composer/composer:latest require --dev phpunit/phpunit ^6.0
---
+++ IntroductiontoWritingUnitTestsinPHPwithPHPUnitandDocker/app/run.test.sh
@@ -0,0 +1,1 @@
+docker run -v $(pwd):/app --rm phpunit/phpunit:latest --bootstrap src/ExampleClass.php tests/ExampleTest.php
