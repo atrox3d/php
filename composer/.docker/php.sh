a16ba81f46bc5d41613bd0b5afa65807a2f59399
administrator <atrox3d@gmail.com>
2018-06-22 18:40:24 +0200

    add composer folder

--- composer/.docker/php.sh
+++ composer/.docker/php.sh
@@ -1,1 +1,2 @@
-docker run --rm -v ${2:-$(pwd)}:/app php:cli php "$1"
+echo docker run --rm --user $(id -u) -v ${2:-$(pwd)}:/app -w ${3:-/app} php:cli php "$1"
+docker run --rm --user $(id -u) -v ${2:-$(pwd)}:/app -w ${3:-/app} php:cli php "$1"
