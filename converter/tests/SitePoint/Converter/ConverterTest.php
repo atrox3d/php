6c8becee258e48006a5a8e76eb241a6bde112b06
atrox3d <atrox3d@gmail.com>
2018-06-24 17:49:22 +0200

    first test

--- converter/tests/SitePoint/Converter/ConverterTest.php
+++ converter/tests/SitePoint/Converter/ConverterTest.php
@@ -6,7 +6,70 @@ use PHPUnit\Framework\TestCase;
 
 class ConverterTest extends TestCase
 {
-		public function testHello() {
-			$this->assertEquals( 'Hello', 'Hell' . 'o' );
-		}
-}
+	public function testHello() 
+	{
+		$this->assertEquals( 'Hello', 'Hell' . 'o' );
+	}
+
+    public function testSimpleConversion() {
+        $input = '{"key":"value","key2":"value2"}';
+        $output = [
+            'key' => 'value',
+            'key2' => 'value2'
+        ];
+        $converter = new \SitePoint\Converter\Converter();
+        $this->assertEquals($output, $converter->convertString($input));
+	}
+
+    public function testMoreComplexConversion()
+    {
+        $input     = '{"key":"value","key2":"value2","some-array":[1,2,3,4,5],"new-object":{"key":"value","key2":"value2"}}';
+        $output    = [
+            'key'        => 'value',
+            'key2'       => 'value2',
+            'some-array' => [1, 2, 3, 4, 5],
+            'new-object' => [
+                'key'  => 'value',
+                'key2' => 'value2',
+            ],
+        ];
+        $converter = new \SitePoint\Converter\Converter();
+        $this->assertEquals($output, $converter->convertString($input));
+    }
+
+    public function testMostComplexConversion()
+    {
+        $input     = '[{"key":"value","key2":"value2","some-array":[1,2,3,4,5],"new-object":{"key":"value","key2":"value2"}},{"key":"value","key2":"value2","some-array":[1,2,3,4,5],"new-object":{"key":"value","key2":"value2"}},{"key":"value","key2":"value2","some-array":[1,2,3,4,5],"new-object":{"key":"value","key2":"value2"}}]';
+        $output    = [
+            [
+                'key'        => 'value',
+                'key2'       => 'value2',
+                'some-array' => [1, 2, 3, 4, 5],
+                'new-object' => [
+                    'key'  => 'value',
+                    'key2' => 'value2',
+                ],
+            ],
+            [
+                'key'        => 'value',
+                'key2'       => 'value2',
+                'some-array' => [1, 2, 3, 4, 5],
+                'new-object' => [
+                    'key'  => 'value',
+                    'key2' => 'value2',
+                ],
+            ],
+            [
+                'key'        => 'value',
+                'key2'       => 'value2',
+                'some-array' => [1, 2, 3, 4, 5],
+                'new-object' => [
+                    'key'  => 'value',
+                    'key2' => 'value2',
+                ],
+            ],
+        ];
+        $converter = new \SitePoint\Converter\Converter();
+        $this->assertEquals($output, $converter->convertString($input));
+    }	
+ }
