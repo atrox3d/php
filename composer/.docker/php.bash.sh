198d00a8ea27177b36b71925dcacbdbee6c3ebc5
atrox3d <atrox3d@gmail.com>
2018-06-24 11:08:40 +0200

    fix script

--- composer/.docker/php.bash.sh
+++ composer/.docker/php.bash.sh
@@ -1,18 +1,2 @@
-command=( 	
-			docker run \
-			--rm \
-			-it \
-			--user $(id -u):$(id -g) \
-			-v /etc/passwd:/etc/passwd \
-			-v $HOME:$HOME \
-			-v ${2:-$(pwd)}:/app \
-			-w ${3:-/app} \
-			php:cli \
-			${1:-/bin/bash}
-#			php \
-#			"$@"
-)
-
-echo ${command[@]}
-
-${command[@]}
+#!/bin/bash
+$(dirname $0)/php.mapuserid.sh /bin/bash
